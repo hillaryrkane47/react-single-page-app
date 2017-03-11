9cc3b497585da43332d0d08986bf067fb54882ce
Hillary Kane <hillaryrkane47@gmail.com>
2017-03-11 13:44:45 -0500

    fixing readme

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,10 +12,9 @@ export default (props) => {
     <div style={styles.container} >
       <h1>Home</h1>
       <img
-        alt="mytko"
+        alt="sky"
         style={styles.img}
-        src="https://s-media-cache-ak0.pinimg.com/736x/5e/4f/d1/5e4fd16a971093d0329dd3181ed9edc7.jpg"
-        className="z-depth-2 hoverable"
+        src="http://i.imgur.com/WSYYkeO.jpg"
       />
     </div>
 
