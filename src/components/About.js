489be9b110b1f2bb10da69525b78a3b45ac25199
Hillary Kane <hillaryrkane47@gmail.com>
2017-03-13 11:25:30 -0400

    fixed build, updating css

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import facts from '../data/facts.js';
+// import '../public/index.css';
 
 class About extends Component {
   render(){
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,19 +3,21 @@ import React from 'react';
 
 
 export default (props) => {
-  const styles = {
-    container: { paddingTop: '50px'},
-    img: {maxHeight: '400px', maxWidth: '400px'}
-  }
+  // const styles = {
+  //   container: { paddingTop: '50px'},
+  //   img: {maxHeight: '400px', maxWidth: '400px'}
+  // }
 
   return (
-    <div style={styles.container} >
-      <h1>Home</h1>
-      <img
-        alt="sky"
-        style={styles.img}
-        src="http://i.imgur.com/WSYYkeO.jpg"
-      />
+    <div className="home">
+      <div className="home-content">
+        <div className="home-content-inner">
+          <h1 className="home-heading">lorem ipsum</h1>
+          <hr/>
+          <p className="home-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
     </div>
 
   );
